8fb938221db7317e0bf0e01c0da8df50a5d0f00c
END <vovk.fox@gmail.com>
2022-01-09 19:20:23 +0200

    ### Final code Added black keys......

--- Virtual Piano/task/src/main.js
+++ Virtual Piano/task/src/main.js
@@ -1,12 +1,16 @@
-const keys = document.querySelectorAll('kbd');
-keys.forEach(x => addEventListener('click', play_note(x.innerText)))
 
-document.addEventListener("keypress", function(event) {
-    if(['a', 's', 'd', 'f', 'g', 'h', 'j'].includes(event.key)){
-        play_note((event.key).toUpperCase())
+document.addEventListener('DOMContentLoaded',() => {
+    const keys = document.querySelectorAll('kbd');
+    keys.forEach(x => addEventListener('click', play_note(x.innerText)))
+
+    document.addEventListener("keypress", function (event) {
+        if (['a', 's', 'd', 'f', 'g', 'h', 'j', 'w', 'e', 't','y', 'u'].includes(event.key)) {
+            play_note((event.key).toUpperCase())
+        }
+    });
+
+    function play_note(key) {
+        return new Audio(`sounds/${key}.mp3`).play()
     }
-});
-function play_note(key) {
-    console.log(key)
-    return new Audio(`sounds/${key}.mp3`).play()
-}
+
+})
