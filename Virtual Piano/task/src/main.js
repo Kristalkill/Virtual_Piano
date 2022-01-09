b50bbffd011fd85da3d895e2e8b3dbf758d347a4
END <vovk.fox@gmail.com>
2022-01-09 18:07:08 +0200

    ### Stage №4 Add keys sound

--- Virtual Piano/task/src/main.js
+++ Virtual Piano/task/src/main.js
@@ -1,5 +1,12 @@
+const keys = document.querySelectorAll('kbd');
+keys.forEach(x => addEventListener('click', play_note(x.innerText)))
+
 document.addEventListener("keypress", function(event) {
     if(['a', 's', 'd', 'f', 'g', 'h', 'j'].includes(event.key)){
-        console.log(`The '${event.key}' key is pressed`)
+        play_note((event.key).toUpperCase())
     }
 });
+function play_note(key) {
+    console.log(key)
+    return new Audio(`sounds/${key}.mp3`).play()
+}
